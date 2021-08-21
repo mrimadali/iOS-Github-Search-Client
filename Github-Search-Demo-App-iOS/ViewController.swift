5fd1c485f05b5f37fc18c4773bb66b39ca08397c
Mohammad Imad Ali <imad@getmuv.com>
2021-08-21 20:25:05 +0530

    Initial Commit

---
+++ Github-Search-Demo-App-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Github-Search-Demo-App-iOS
+//
+//  Created by Mohammad Imad Ali on 21/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
